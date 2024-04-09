7a8b6c827613c42d424751b7955a6428b8466146
Ali Al-Zobaydee <142319718+epic41z0@users.noreply.github.com>
2024-04-09 14:48:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,35 +1,13 @@
-#include <avr/io.h>
 #include "serial.h"
 
-void uart_init() {
-    // Konfigurera baud rate till 9600
-    UBRR0H = (unsigned char)(103 >> 8);
-    UBRR0L = (unsigned char)103;
-    // Aktivera sändning och mottagning
-    UCSR0B = (1 << TXEN0) | (1 << RXEN0);
-    // Ställ in frame format: 8 data, 1 stop bit
-    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
-}
+int main() {
+    // Initiera UART
+    uart_init();
 
-char uart_getchar() {
-    // Vänta på att ett tecken ska mottagas
-    while (!(UCSR0A & (1 << RXC0)))
-        ;
-    // Returnera tecknet som mottogs
-    return UDR0;
-}
+    while (1) {
+        // Anropa uart_echo för att vänta på och skicka tillbaka ett inkommande tecken
+        uart_echo();
+    }
 
-void uart_putchar(char data) {
-    // Vänta på att bufferten ska bli klar för sändning
-    while (!(UCSR0A & (1 << UDRE0)))
-        ;
-    // Lägg till data i sändningsbufferten
-    UDR0 = data;
-}
-
-void uart_echo() {
-    // Vänta på ett inkommande tecken
-    char received_char = uart_getchar();
-    // Skicka tillbaka samma tecken
-    uart_putchar(received_char);
+    return 0;
 }
