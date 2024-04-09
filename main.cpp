8de8c29c56a4004e2cd3098ff6d851e4d56dae37
Ali Al-Zobaydee <142319718+epic41z0@users.noreply.github.com>
2024-04-09 14:03:07 +0200

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,28 +1,20 @@
 #include <avr/io.h>
-
-#define LED_PIN 8
-#define BUTTON_PIN 9
+#include <util/delay.h>
+#include "serial.h"
 
 int main() {
-    // Konfigurera LED_PIN som output
-    DDRB |= (1 << LED_PIN);
+    // Initiera UART
+    uart_init();
 
-    // Konfigurera BUTTON_PIN som input
-    DDRB &= ~(1 << BUTTON_PIN);
-    // Aktivera pull-up motstånd för knappen
-    PORTB |= (1 << BUTTON_PIN);
+    // Anropa uart_putstr för att skicka en sträng över UART
+    uart_putstr("Hello, world!");
 
     while (1) {
-        // Läs status för knappen
-        if (PINB & (1 << BUTTON_PIN)) {
-            // Om knappen är tryckt, sätt LED_PIN till HIGH
-            PORTB |= (1 << LED_PIN);
-        } else {
-            // Annars, sätt LED_PIN till LOW
-            PORTB &= ~(1 << LED_PIN);
-        }
+        // Anropa uart_putchar för att skicka ett enskilt tecken över UART
+        uart_putchar('A');
+        // Vänta 1 sekund
+        _delay_ms(1000);
     }
-  
 
     return 0;
 }
